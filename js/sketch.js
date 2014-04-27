fe73490b52f8cb63fed3256d21b33228c28a397b
Alan Russell <ajrussellaudio@gmail.com>
2014-04-27 22:38:49 +0100

    done with this for now

--- js/sketch.js
+++ js/sketch.js
@@ -1,24 +1,29 @@
 var containerSize = 960;
-var numSquares = 16;
+var numSquares = prompt("How many squares on each side? \nSuggested: 12-36", 16);
 var squares = [ ];
 
+// Fill array with HTML squares
 var makeSquares = function(size) {
 	for (var i = 0; i < (size * size); i++) {
 		squares.push('<div class="square"></div>');
 	};
 };
 
+// get squares' dimensions from size of container
 var sqDimensions = function() {
 	return((containerSize / numSquares) - 2 + "px");
 }
 
+// make squares
 makeSquares(numSquares);
-// console.log(squares);
 
 $(document).ready (function() {
+	// hide initial elements
 	$("h1").addClass("hidden");
 	$("#checkScript").addClass("hidden");
-	$("#options").removeClass("hidden");
+
+	// make scripted elements
+	$("body").append('<div id="options"><button id="btn_clear">Clear screen</button></div>');
 	$("body").append('<div id="container"></div>');
 	$("#container").css({
 		width: containerSize + "px"
@@ -30,9 +35,14 @@ $(document).ready (function() {
 		height: sqDimensions,
 		width: sqDimensions
 	});
+
+	// make interactive elements
 	$(".square").mouseenter( function() {
 		$(this).addClass("colored");
 	});
+	// $(".square").mouseleave( function() {
+	// 	$(this).removeClass("colored");
+	// });
 	$("#btn_clear").click(function() {
 		$("#container .square").removeClass("colored");
 	})
