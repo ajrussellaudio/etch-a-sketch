c0a4dced18dde67de229b924df23bc30c1264441
Alan Russell <ajrussellaudio@gmail.com>
2014-04-27 20:31:24 +0100

    update first prototype

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,37 @@
+var containerSize = 960;
+var numSquares = 16;
+var squares = [ ];
+
+var makeSquares = function(size) {
+	for (var i = 0; i < (size * size); i++) {
+		squares.push('<div class="square"></div>');
+	};
+};
+
+var sqDimensions = function() {
+	return((containerSize / numSquares) - 2 + "px");
+}
+
+makeSquares(numSquares);
+// console.log(squares);
+
 $(document).ready (function() {
 	$("#checkScript").remove();
-	$("body").append('<div id="container"></div>')
+	$("body").append('<div id="container"></div>');
+	$("#container").css({
+		width: containerSize + "px"
+	})
+	for (var i = 0; i < squares.length; i++) {
+		$("#container").append(squares[i]);
+	};
+	$(".square").css({
+		height: sqDimensions,
+		width: sqDimensions
+	});
+	$(".square").mouseenter( function() {
+		$(this).css({
+			"background-color": "#f00"
+		});
+	});
 });
+
